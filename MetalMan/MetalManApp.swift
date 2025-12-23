411690cb42aa5f43fcbe8bba9f7d3833f163f258
Max Davis <max@maxportland.com>
2025-12-23 15:12:31 -0500

    Initial Commit

---
+++ MetalMan/MetalManApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalManApp.swift
+//  MetalMan
+//
+//  Created by Max Davis on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MetalManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
